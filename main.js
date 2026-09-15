def09f1afca639ff9441de20f80e32110189bce2
123marlinocomo <123marlinocomo@gmail.com>
2026-09-15 23:43:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,56 +1,50 @@
-<div class="row">
-    <div class="col-12">
+<form id="contactForm">
 
-        <div class="contact-form-area">
-            <form id="contactForm" action="#" method="post">
+    <div class="row">
 
-                <div class="row">
+        <div class="col-lg-4">
+            <div class="form-group">
+                <input type="text" class="form-control" id="name" placeholder="Name">
+                <span id="erroNome" class="erro"></span>
+            </div>
+        </div>
 
-                    <div class="col-lg-4">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="name" placeholder="Name">
-                            <span id="erroNome" class="erro"></span>
-                        </div>
-                    </div>
+        <div class="col-lg-4">
+            <div class="form-group">
+                <input type="text" class="form-control" id="email" placeholder="E-mail">
+                <span id="erroEmail" class="erro"></span>
+            </div>
+        </div>
 
-                    <div class="col-lg-4">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="email" placeholder="E-mail">
-                            <span id="erroEmail" class="erro"></span>
-                        </div>
-                    </div>
+        <div class="col-lg-4">
+            <div class="form-group">
+                <input type="text" class="form-control" id="subject" placeholder="Subject">
+                <span id="erroAssunto" class="erro"></span>
+            </div>
+        </div>
 
-                    <div class="col-lg-4">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="subject" placeholder="Subject">
-                            <span id="erroAssunto" class="erro"></span>
-                        </div>
-                    </div>
+        <div class="col-12">
+            <div class="form-group">
+                <textarea
+                    name="message"
+                    class="form-control"
+                    id="message"
+                    cols="30"
+                    rows="10"
+                    placeholder="Message"></textarea>
 
-                    <div class="col-12">
-                        <div class="form-group">
-                            <textarea 
-                                name="message" 
-                                class="form-control" 
-                                id="message" 
-                                cols="30" 
-                                rows="10" 
-                                placeholder="Message">
-                            </textarea>
-                            <span id="erroMensagem" class="erro"></span>
-                        </div>
-                    </div>
+                <span id="erroMensagem" class="erro"></span>
+            </div>
+        </div>
 
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary">
-                            Enviar
-                        </button>
-                    </div>
+        <div class="col-12">
+            <div id="mensagemSucesso"></div>
 
-                </div>
-
-            </form>
+            <button type="submit" class="btn btn-primary">
+                Submeter
+            </button>
         </div>
 
     </div>
-</div>
+
+</form>
